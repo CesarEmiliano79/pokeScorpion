6fc1afd1a63d49bcb9ca6a274f06c4fad1765f3c
CesarEmiliano79 <cesaremi12rjpz@gmail.com>
2025-09-25 23:36:09 -0600

    Cambio en el gitignore

--- frontend/.next/server/chunks/ssr/src_app_ca777385._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[68758,a=>{a.v("/_next/static/media/favicon.93c4c4b8.ico")},19362,a=>{"use strict";a.s(["default",()=>b]);let b={src:a.i(68758).default,width:500,height:500}}];
-
-//# sourceMappingURL=src_app_ca777385._.js.map
--- frontend/.next/static/chunks/b005311cd47821a3.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/2a27418d9975c565.js",
-  "static/chunks/turbopack-f8fbd7eec892b5d6.js"
-])
